d48ef22c65678eddcd2eea6f165481e354432b53
Magnus <magnus.sunesson@outlook.com>
2026-09-05 15:36:08 +0200

    program, add  services for DI

---
+++ FeedrBackend/FeedrBackend/Program.cs
@@ -0,0 +1,26 @@
+using Azure.Monitor.OpenTelemetry.Exporter;
+using FeedrBackend.Clients.Interface;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Azure.Functions.Worker.OpenTelemetry;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OpenTelemetry;
+
+var builder = FunctionsApplication.CreateBuilder(args);
+
+builder.ConfigureFunctionsWebApplication();
+
+builder.Services.AddHttpClient<IFeedrServiceApiClient, FeedrServiceApiClient>("FeedrService", c =>
+{
+    c.BaseAddress = new Uri("http://localhost:7071/api/postFeed");
+});
+
+if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING")))
+{
+    builder.Services.AddOpenTelemetry()
+        .UseFunctionsWorkerDefaults()
+        .UseAzureMonitorExporter();
+}
+
+builder.Build().Run();
