2331efa67b7005f3d4a82d323819295daa884cb0
Magnus <magnus.sunesson@outlook.com>
2026-09-05 15:34:42 +0200

    Api client posting to the service api

---
+++ FeedrBackend/FeedrBackend/Clients/Client/FeedrServiceApiClient.cs
@@ -0,0 +1,21 @@
+﻿using FeedrBackend.Clients.Interface;
+using FeedrBackend.Models;
+using System.Net.Http.Json;
+
+public class FeedrServiceApiClient : IFeedrServiceApiClient
+{
+    private readonly HttpClient _httpClient;
+
+    public FeedrServiceApiClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+    }
+
+    public async Task<HttpResponseMessage> PostFeed(FeedModel feed)
+    {
+        ArgumentNullException.ThrowIfNull(feed);
+        var response = await _httpClient.PostAsJsonAsync("api/postFeed", feed).ConfigureAwait(false);
+        response.EnsureSuccessStatusCode();
+        return response;
+    }
+}
---
+++ FeedrBackend/FeedrBackend/Clients/Interface/IFeedrServiceApiClient.cs
@@ -0,0 +1,12 @@
+﻿using FeedrBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FeedrBackend.Clients.Interface
+{
+    public interface IFeedrServiceApiClient
+    {
+        public Task<HttpResponseMessage> PostFeed(FeedModel feed);
+    }
+}
